6c2e0a46bec2e5cb3c8c9aaf9eebf66844d0c651
DeejayRevok <seryi_one@hotmail.com>
2020-06-16 21:15:20 +0200

    First commit

---
+++ webapp/main.py
@@ -0,0 +1,53 @@
+"""
+Application main module
+"""
+import sys
+
+from aiohttp.web_app import Application
+from aiohttp_apispec import validation_middleware
+from news_service_lib import HealthCheck, server_runner
+
+from infrastructure.storage.db_initializer import initialize_db
+from infrastructure.storage.sql_storage import SqlStorage
+from log_config import LOG_CONFIG, get_logger
+from services.authentication_service import AuthService
+from services.users_service import UserService
+from webapp.definitions import API_VERSION, CONFIG_PATH, health_check
+from webapp.middlewares import error_middleware, auth_middleware
+from webapp.views import users_view, auth_view
+
+
+def init_uaa(app: Application) -> Application:
+    """
+    Initialize the web application
+
+    Args:
+        app: configuration profile to use
+
+    Returns: web application initialized
+    """
+    storage_config = app['config'].get_section('storage')
+
+    store_client = SqlStorage(**storage_config)
+
+    if not store_client.health_check():
+        sys.exit(1)
+
+    initialize_db(store_client.engine)
+    app['user_service'] = UserService(store_client)
+    app['auth_service'] = AuthService(app['user_service'])
+
+    HealthCheck(app, health_check)
+
+    users_view.setup_routes(app)
+    auth_view.setup_routes(app)
+
+    app.middlewares.append(error_middleware)
+    app.middlewares.append(auth_middleware)
+    app.middlewares.append(validation_middleware)
+
+    return app
+
+
+if __name__ == '__main__':
+    server_runner('UAA', init_uaa, API_VERSION, CONFIG_PATH, LOG_CONFIG, get_logger)
